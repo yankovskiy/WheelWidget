b12db75e0fc53cbfa9aea8c8268083e7425b9078
Artem Yankovskiy <artemyankovskiy@gmail.com>
2013-11-28 16:42:19 +1100

    Тестовый дизайн для Экспозиционных пар

---
+++ EVtest/src/com/example/evtest/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.evtest;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends SherlockActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
