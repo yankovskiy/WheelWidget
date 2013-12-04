a131d5634bc0658f24abd4583e84fc21c75c389b
Artem Yankovskiy <artemyankovskiy@gmail.com>
2013-12-04 17:10:32 +1100

    Набросок дизайна для ГРИП-калькулятора

---
+++ doftest/src/com/example/doftest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.doftest;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends SherlockActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
